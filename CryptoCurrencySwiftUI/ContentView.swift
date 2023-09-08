c675f6dba03be5946817c1e18d4a35a4e7b1c5d2
YOGESH TEKWANI <61420707+Mr-Yogesh-Tekwani@users.noreply.github.com>
2023-09-08 15:37:01 -0400

    Initial Commit

---
+++ CryptoCurrencySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoCurrencySwiftUI
+//
+//  Created by Yogesh on 9/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoCurrencySwiftUI/CryptoCurrencySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencySwiftUIApp.swift
+//  CryptoCurrencySwiftUI
+//
+//  Created by Yogesh on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
